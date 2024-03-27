71af4f3491e69cc7f47c922373ffe83ecaaf8212
Your Name <you@example.com>
2024-03-27 22:40:53 +0000

    new try

--- list.c
+++ list.c
@@ -134,30 +134,23 @@ void * popBack(List * list) {
 }
 
 void * popCurrent(List * list) {
-
-  if (list->current == NULL){
-    list->current = NULL;
-    list->head = NULL;
-    list->tail = NULL;
+  
+  Nodo* nodoEliminar = list->current;
+  if (list->current->next != NULL){
+    list->current->next->prev = list->current->prev;
   }
   else{
-    Nodo* nodoEliminar = list->current;
-    if (list->current->next != NULL){
-      list->current->next->prev = list->current->prev;
-    }
-    else{
-      list->tail = list->current->prev;
-    }
-
-    if (list->current->prev != NULL){
-      list->current->prev->next = list->current->next;
-    }
-    else{
-      list->head = list->current->next;
-    }
-    free(nodoEliminar);
-    return nodoEliminar->data;
+    list->tail = list->current->prev;
   }
+
+  if (list->current->prev != NULL){
+    list->current->prev->next = list->current->next;
+  }
+  else{
+    list->head = list->current->next;
+  }
+  free(nodoEliminar);
+  return nodoEliminar->data;
 }
 
 void cleanList(List * list) {
